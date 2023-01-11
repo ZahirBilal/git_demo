4a3d9c039e4c7c951f6bb1c87b4046df62dab49c
Zahir Bilal <zaherbilal93@gmail.com>
2023-01-11 12:39:38 +0100

    feature_updated_readme

---
+++ git_commands.py
@@ -0,0 +1,19 @@
+git init
+git add .
+git add file_name.ext
+git commit -m "message_optional" -m "description_optional"
+git status
+git remote add origin "ssh_link"
+git remote -v
+git push -u origin master # -u defines the upstream, afterwards only git push
+git push
+git 
+
+
+## to link the local machine with github using ssh
+ssh-add ~/.ssh/zahir_ssh # zahir_ssh = name of the dir where public key
+
+#branching 
+git branch
+git checkout -b branch_name # to create and switch between branches
+
